8f0e001b2e1d661b0a3d73b81d7b2bf7f3d93964
Andrey <trishichev@gmail.com>
2021-05-06 13:11:37 +0300

    2

--- sqliter3.py
+++ sqliter3.py
@@ -9,20 +9,20 @@ class Sqlighter:
 
     def get_subscription(self, status=True):
         with self.connection:
-            return self.cursor.execute(status).fetchall()
+            return self.cursor.execute("SELECT FROM subscription WHERE status = ?", (status)).fetchall()
 
     def subscriber_exists(self, user_id):
         with self.connection:
-            result = self.cursor.execute(user_id).fetchall()
+            result = self.cursor.execute("SELECT FROM subscription WHERE user_id = ?", (user_id)).fetchall()
             return bool(len(result))
 
     def add_subscriber(self, user_id, status=True):
         with self.connection:
-            return self.cursor.execute(user_id, status)
+            return self.cursor.execute("INSERT INTO subscription (user_id, status) VALUES (?/?)", (user_id, status))
 
     def update_subscription(self, user_id, status):
         """Обновляем статус подписки"""
-        return self.cursor.execute(user_id, status)
+        return self.cursor.execute("UPDATE subscrition SET status = ? WHERE user_id = ?", (status, user_id))
 
     def close(self):
         self.connection.close()
